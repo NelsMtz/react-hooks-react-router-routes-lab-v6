b04d09d0569abd9e3c436cdce8eab72cb55e71b2
Nelly <nmatu308@gmail.com>
2024-08-09 02:24:13 +0300

    first attempt

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "../pages/Home";
+import Actors from "./Actors";
+import Directors from "../pages/Directors";
+import Movies from "./Movies";
+
+function App() {
+	return (
+		<div>
+			<NavBar />
+
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route exact path="/movies">
+					<Movies />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route exact path="/directors">
+					<Directors />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route exact path="/actors">
+					<Actors />
+				</Route>
+			</Switch>
+		</div>
+	);
+}
+
+export default App;
